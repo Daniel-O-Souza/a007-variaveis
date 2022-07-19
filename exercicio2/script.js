6168d00c70dc7fc5b8dccaf25c36cf0914d214ff
Daniel <daniel.sprcp@gmail.com>
2022-07-19 13:49:27 -0300

    Feito exercicio 2

---
+++ exercicio2/script.js
@@ -0,0 +1,51 @@
+// PERGUNTAS
+
+ const pergunta1 = prompt("Você Gosta de churrasco?")
+
+ const pergunta2 = prompt("Você gosta de animais?")
+
+ const pergunta3 = prompt("Você gosta de cerveja?")
+
+// ITEM A: PESPOSTAS
+
+ const resposta1 = pergunta1
+
+ const resposta2 = pergunta2
+
+ const resposta3 = pergunta3
+
+// ITEM B: IMPRIMINDO RESPOSTAS NA TELA
+
+// console.log(resposta1)
+
+// console.log(resposta2)
+
+// console.log(resposta3)
+
+// ITEM C: ALTERANDO PROGRAMA PARA QUE TEXTO DAS PERGUNTAS SEJA GUARDADO EM VARIAVEIS
+
+// const pergunta1 = ("Você gosta de churrasco?")
+// const resposta1 = prompt(pergunta1)
+
+// const pergunta2 = ("Você gosta de animais?")
+// const resposta2 = prompt(pergunta2)
+
+// const pergunta3 = ("Você gosta de cerveja?")
+// const resposta3 = prompt(pergunta3)
+
+// ITEM D: ALTERANDO PROGRAMA PARA QUE IMPRESSÕES DAS RESPOSTAS TAMBÉM TENHAM AS PERGUNTAS
+
+console.log("Você gosta de churrasco?", resposta1)
+
+console.log("Você gosta de animais?", resposta2)
+
+console.log("Você gosta de cerveja", resposta3)
+
+
+
+
+
+
+
+
+
